7699640adc26d89f34ea377232685f2ca0584c7d
pedro <pbbdasilva@gmail.com>
2023-12-03 20:14:23 -0300

    add suballocations to small allocations (< page size)

--- mmap-bins/pbmalloc.c
+++ mmap-bins/pbmalloc.c
@@ -1,10 +1,4 @@
-//
-// Created by pedro on 02/12/23.
-//
-//TO-DO: solve >= 320 bytes allocation to behave as old allocation (split_block, etc)
-
-#include <sys/mman.h>
-#include "pbmalloc-binned.h"
+#include "pbmalloc.h"
 
 Chunk* bins_ptr[NUM_BINS] = {0};
 
@@ -16,12 +10,16 @@ static inline int WIDE_ENOUGH(Chunk* ptr, size_t size) {
     return ptr->size >= size;
 }
 
-static inline size_t PAGE_SIZE(size_t size) {
+static inline size_t BIN_ALIGN(size_t size) {
     return ALIGN32(size + METADATA_SIZE);
 }
 
+static inline size_t PAGE_ALIGN(size_t size) {
+    return ALIGNPAGE(size + METADATA_SIZE);
+}
+
 Chunk* find_block(Chunk** last, size_t size) {
-    size_t page_index = (PAGE_SIZE(size)/BIN_SIZE) - 1;
+    size_t page_index = (BIN_ALIGN(size) / BIN_SIZE) - 1;
     Chunk* ptr = bins_ptr[page_index];
 
     // search for free chunk with enough size
@@ -35,24 +33,42 @@ Chunk* find_block(Chunk** last, size_t size) {
     return ptr;
 }
 
+void split_block(Chunk* chunk, size_t size) {
+    Chunk* spare_chunk = (void*) chunk + BIN_ALIGN(size);
+    spare_chunk->size = chunk->size - BIN_ALIGN(size);
+    spare_chunk->next = chunk->next;
+    spare_chunk->prev = chunk;
+    spare_chunk->free = 1;
+
+    chunk->next = spare_chunk;
+    chunk->size = size;
+}
+
 Chunk* alloc_memory(Chunk* last, size_t size) {
-    void* block = mmap(NULL, PAGE_SIZE(size), PROT_WRITE | PROT_READ,
-                       MAP_PRIVATE | MAP_ANONYMOUS,0, 0);
+    size_t allocated_size = PAGE_ALIGN(size);
+    void* block = mmap(NULL, allocated_size, PROT_WRITE | PROT_READ,
+                       MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
     assert(block != MAP_FAILED);
+
     Chunk* new_block = block;
-    new_block->size = PAGE_SIZE(size) - METADATA_SIZE;
+    new_block->size = allocated_size - METADATA_SIZE;
     new_block->next = NULL;
+
     if (last != NULL) {
         last->next = new_block;
         new_block->prev = last;
     }
     new_block->free = 0;
 
+    if (BIN_ALIGN(size) <= (PAGE_SIZE >> 1)) {
+        split_block(new_block, ALIGN32(size));
+    }
+
     return new_block;
 }
 
 void* pbmalloc(size_t size) {
-    size_t page_index = (PAGE_SIZE(size) / BIN_SIZE) - 1;
+    size_t page_index = MIN((BIN_ALIGN(size) / BIN_SIZE) - 1, 127);
     Chunk* curr = NULL;
     Chunk* last = NULL;
 
@@ -61,6 +77,7 @@ void* pbmalloc(size_t size) {
         curr = find_block(&last, size);
 
         if (curr != NULL) {
+            if (curr->size >= 2*BIN_ALIGN(size)) split_block(curr, ALIGN32(size));
             curr->free = 0;
         } else {
             curr = alloc_memory(last, size);
--- mmap-bins/pbmalloc.h
+++ mmap-bins/pbmalloc.h
@@ -1,19 +1,21 @@
-//
-// Created by pedro on 02/12/23.
-//
-
-#ifndef PB_MALLOC_PBMALLOC_BINNED_H
-#define PB_MALLOC_PBMALLOC_BINNED_H
+#ifndef PB_MALLOC_PBMALLOC_H
+#define PB_MALLOC_PBMALLOC_H
 
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+#include <sys/mman.h>
+#include <math.h>
 #include <unistd.h>
 
 // >= multiple of 32. then, result >= 32
+#define NUM_BINS 128
 #define BIN_SIZE 32
-#define NUM_BINS 10
-#define ALIGN32(x) (((x+31) >> 5) << 5)
+#define ALIGN32(x) (((x + BIN_SIZE - 1) >> 5) << 5)
+#define PAGE_SIZE 4096
+#define ALIGNPAGE(x) (((x + PAGE_SIZE - 1) >> 12) << 12)
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 typedef struct Chunk {
     size_t size;
@@ -27,4 +29,4 @@ typedef struct Chunk {
 void* pbmalloc(size_t size);
 void pbfree(void* ptr);
 
-#endif //PB_MALLOC_PBMALLOC_BINNED_H
+#endif //PB_MALLOC_PBMALLOC_H
