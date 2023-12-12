18286d62348919dd9765da314391900e0cbcd95a
pedro <pbbdasilva@gmail.com>
2023-12-12 13:31:10 -0300

    update benchmark

--- benchmark.c
+++ benchmark.c
@@ -1,4 +1,4 @@
-#include "mmap-bins/pbmalloc.h"
+#include "pbmalloc.h"
 #include <stdio.h>
 #include <time.h>
 
@@ -6,29 +6,75 @@
 #define ARRAY_SIZE 1000
 #define STRIDE 7
 
-void test() {
+clock_t begin, end;
+
+void test_pbmalloc() {
     int* v[NUM_ROUNDS];
+    size_t size[NUM_ROUNDS];
+
+    begin = clock();
     for(size_t i = 0; i < NUM_ROUNDS; i++) {
-        v[i] = malloc(ARRAY_SIZE * sizeof(int));
+        size_t v_size = (rand() % ARRAY_SIZE);
+        v[i] = pbmalloc(v_size * sizeof(int));
+        size[i] = v_size;
     }
 
     for(size_t i = 0; i < NUM_ROUNDS; i++) {
-        for(size_t j = 0; j < ARRAY_SIZE; j++)
+        for(size_t j = 0; j < size[i]; j++)
             v[i][j] = rand();
     }
 
     for(size_t i = 0; i < NUM_ROUNDS; i+=STRIDE)
         pbfree(v[i]);
 
-    for(size_t i = 0; i < NUM_ROUNDS; i += 7) {
-        v[i] = malloc(ARRAY_SIZE * sizeof(int));
+    for(size_t i = 0; i < NUM_ROUNDS; i += STRIDE) {
+        size_t v_size = (rand() % ARRAY_SIZE);
+        v[i] = pbmalloc(v_size * sizeof(int));
+        size[i] = v_size;
     }
+
+    for(size_t i = 0; i < NUM_ROUNDS; i++)
+        pbfree(v[i]);
+
+    end = clock();
+}
+
+void test_malloc() {
+    int* v[NUM_ROUNDS];
+    size_t size[NUM_ROUNDS];
+
+    begin = clock();
+    for(size_t i = 0; i < NUM_ROUNDS; i++) {
+        size_t v_size = (rand() % ARRAY_SIZE);
+        v[i] = malloc(v_size * sizeof(int));
+        size[i] = v_size;
+    }
+
+    for(size_t i = 0; i < NUM_ROUNDS; i++) {
+        for(size_t j = 0; j < size[i]; j++)
+            v[i][j] = rand();
+    }
+
+    for(size_t i = 0; i < NUM_ROUNDS; i+=STRIDE)
+        free(v[i]);
+
+    for(size_t i = 0; i < NUM_ROUNDS; i += STRIDE) {
+        size_t v_size = (rand() % ARRAY_SIZE);
+        v[i] = malloc(v_size * sizeof(int));
+        size[i] = v_size;
+    }
+
+    for(size_t i = 0; i < NUM_ROUNDS; i++)
+        free(v[i]);
+
+    end = clock();
 }
 
 int main() {
-    clock_t begin = clock();
-    test();
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;\
-    printf("%f", time_spent);
+//    test_malloc();
+//    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+//    printf("%f\n", time_spent);
+    test_pbmalloc();
+//    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+//    printf("%f\n", time_spent);
 }
